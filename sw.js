e21d598cc9ef82d5a1a1b013eb966932f3ec577e
Robin Strik <robin.strik@ru.nl>
2025-06-01 02:34:15 +0200

    Trying to make the PWA work

---
+++ sw.js
@@ -0,0 +1,64 @@
+self.addEventListener("install", function(event) {
+  event.waitUntil(preLoad());
+});
+
+var preLoad = function(){
+  console.log("Installing web app");
+  return caches.open("offline").then(function(cache) {
+    console.log("caching index and important routes");
+    return cache.addAll([
+      "/", 
+      "/index.html",
+      "/offline.html",
+      "/manifest.json",
+      "/css/style.css", 
+      "/assets/favicon-16x16.png", 
+      "/assets/favicon-32x32.png", 
+      "/assets/icon-192.png", 
+      "/assets/icon-512.png", 
+      "/data/buildings.geojson", 
+      "/js/map.js",
+      "/plugins/leaflet.edgebuffer.js"
+    ]);
+  });
+};
+
+self.addEventListener("fetch", function(event) {
+  event.respondWith(checkResponse(event.request).catch(function() {
+    return returnFromCache(event.request);
+  }));
+  event.waitUntil(addToCache(event.request));
+});
+
+var checkResponse = function(request){
+  return new Promise(function(fulfill, reject) {
+    fetch(request).then(function(response){
+      if(response.status !== 404) {
+        fulfill(response);
+      } else {
+        reject();
+      }
+    }, reject);
+  });
+};
+
+var addToCache = function(request){
+  return caches.open("offline").then(function (cache) {
+    return fetch(request).then(function (response) {
+      console.log(response.url + " was cached");
+      return cache.put(request, response);
+    });
+  });
+};
+
+var returnFromCache = function(request){
+  return caches.open("offline").then(function (cache) {
+    return cache.match(request).then(function (matching) {
+     if(!matching || matching.status == 404) {
+       return cache.match("offline.html");
+     } else {
+       return matching;
+     }
+    });
+  });
+};
