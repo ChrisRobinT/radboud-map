62e590ff54fbdbf0803b801055f2b0e8530e0201
Gregor Bujda <gregor@Gregors-MacBook-Pro.local>
2025-06-03 18:37:30 +0200

    removed some unused variables in updateInfoPanel

--- src/infoPanel.js
+++ src/infoPanel.js
@@ -1,13 +1,9 @@
 import {
-    INFO_PANEL_ID,
-    UP_BUTTON_ID,
-    DOWN_BUTTON_ID
+    INFO_PANEL_ID
 } from './config.js';
 
 export function updateInfoPanel(content) {
     const infoPanel = document.getElementById(INFO_PANEL_ID);
-    const upButton = document.getElementById(UP_BUTTON_ID);
-    const downButton = document.getElementById(DOWN_BUTTON_ID);
     const buildingTools = document.getElementById('buildingTools');
 
 
